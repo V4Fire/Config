6eb13424030d6fe8755df77cdd82c1163c561532
kholstinin <kholstinin@yandex-team.ru>
2021-11-24 18:46:40 +0500

    added webpack --env support

--- core/argv.js
+++ core/argv.js
@@ -1,26 +1,6 @@
 'use strict';
 
 const {argv} = process;
+const parseArgs = require('./parseArgs');
 
-function isFlag(str) {
-	return /^--?[\S]+$/.test(str);
-}
-
-module.exports = argv.reduce((parsed, arg, i, argv) => {
-	if (isFlag(arg)) {
-		const next = argv[i + 1];
-
-		let value;
-
-		if (next && !isFlag(next)) {
-			value = next;
-
-		} else {
-			value = !/^--no-/.test(arg);
-		}
-
-		parsed[arg.replace(/^--?(?:no-)?/, '')] = value;
-	}
-
-	return parsed;
-}, {});
+module.exports = parseArgs(argv);
